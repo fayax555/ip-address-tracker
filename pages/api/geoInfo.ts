cd31db88223832bb681d335710ec7d3ad5d1cd74
fayax555 <42441836+fayax555@users.noreply.github.com>
2022-02-06 21:26:50 +0500

    get client's ip instead of server ip

--- pages/api/geoInfo.ts
+++ pages/api/geoInfo.ts
@@ -7,15 +7,17 @@ export default async function handler(
 ) {
   const { GEO_API_KEY } = process.env
   const detectedIp = req.headers['x-real-ip'] || req.socket.remoteAddress
-  const { ip = detectedIp } = req.body
-  const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${GEO_API_KEY}&domain=${ip}`
+  const { ip } = req.body
+  const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${GEO_API_KEY}&domain=${
+    ip ? ip : detectedIp
+  }`
   const response = await fetch(url)
   const data: GeoData = await response.json()
 
-  console.log(detectedIp)
-
   if (!response.ok) {
-    res.status(400).json({ message: 'Enter correct IP address or domain' })
+    return res
+      .status(400)
+      .json({ message: 'Enter correct IP address or domain' })
   }
 
   res.status(200).json(data)
