54b4814493e43f3f0f5cb4571d62c9225e99e592
fayax555 <42441836+fayax555@users.noreply.github.com>
2022-02-06 20:17:32 +0500

    update

--- pages/api/geoInfo.ts
+++ pages/api/geoInfo.ts
@@ -6,10 +6,13 @@ export default async function handler(
   res: NextApiResponse<GeoData | { message: string }>
 ) {
   const { GEO_API_KEY } = process.env
-  const { ip = '' } = req.body
+  const detectedIp = req.headers['x-real-ip'] || req.socket.remoteAddress
+  const { ip = detectedIp } = req.body
   const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${GEO_API_KEY}&domain=${ip}`
   const response = await fetch(url)
   const data: GeoData = await response.json()
+
+  console.log(detectedIp)
 
   if (!response.ok) {
     res.status(400).json({ message: 'Enter correct IP address or domain' })
